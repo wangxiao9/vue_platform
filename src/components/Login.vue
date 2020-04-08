5e487d0d127c7a16c03aa52bf74767310bdccb59
unknown <1012883981@qq.com>
2020-04-08 19:23:38 +0800

    login 样式完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,10 +1,79 @@
 <template>
-  <div>登录组件</div>
+  <div class="login_container">
+    <!--定义一个登入盒子-->
+    <div class="login_box">
+      <!--头像-->
+      <div class="logo_box">
+        <img src="../assets/logo.png" />
+      </div>
+      <!--登录表单-->
+      <div>
+        <el-form class="loginForm">
+          <el-form-item>
+            <el-input prefix-icon="el-icon-user"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input prefix-icon="el-icon-lock"></el-input>
+          </el-form-item>
+          <!--按钮-->
+          <el-form-item class="btns">
+            <el-button type="primary">登录</el-button>
+            <el-button type="info">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+.login_container {
+  background: #2b4b6b;
+  height: 100%;
+}
+
+.login_box {
+  width: 450px;
+  height: 300px;
+  background: #ffff;
+  border: 1px solid;
+  border-radius: 2%;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .logo_box {
+    height: 130px;
+    width: 130px;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 50%;
+    background-color: #fff;
+    box-shadow: 0 0 10px #ddd;
+    img {
+      height: 100%;
+      width: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+
+.loginForm {
+  position: absolute;
+  padding: 0 20px;
+  width: 100%;
+  box-sizing: border-box;
+  bottom: 10px;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;  
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form} from 'element-ui'
+import { FormItem } from 'element-ui'
+import { Input } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
