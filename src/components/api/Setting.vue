9e2d9247f2531509a50ab05472192757bd726f66
z00406ae <xiaoqian.zhang.ext@siemens.com>
2020-06-29 17:04:35 +0800

    api

--- src/components/api/Setting.vue
+++ src/components/api/Setting.vue
@@ -14,13 +14,20 @@
           </el-input>
         </el-col>
         <el-col :span="4">
-          <el-button type="primary">添加API配置</el-button>
+          <el-button type="primary" @click="addDrawer=true">添加API配置</el-button>
         </el-col>
       </el-row>
       <!--主体-->
       <el-container>
         <el-aside style="width:500px">
-          <el-table :data="configList" stripe @row-click="openConfigDetail">
+          <el-table
+            :data="configList"
+            stripe
+            @row-click="openConfigDetail"
+            v-loading="loading"
+            element-loading-spinner="el-icon-loading"
+            element-loading-background="rgba(0, 0, 0, 0.8)"
+          >
             <el-table-column prop="name" label="名称"></el-table-column>
             <el-table-column prop="description" label="描述"></el-table-column>
             <el-table-column prop="ConfigStatus" label="状态">
@@ -70,17 +77,58 @@
           <el-divider></el-divider>
           <el-row>
             <el-col>
-              <el-button>发布</el-button>
-              <el-button type="primary">编辑</el-button>
+              <el-button @click="release">发布</el-button>
+              <el-button type="primary" @click="edit">编辑</el-button>
             </el-col>
           </el-row>
         </el-main>
       </el-container>
     </el-card>
+    <!--新增详情页面-->
+    <el-drawer :visible.sync="addDrawer">
+      <div class="drawer_body">
+        <el-form ref="saveConfigFormRef" :model="saveConfigForm" :rules="saveConfigFormRule">
+          <el-form-item prop="name" label="| 配置名称">
+            <el-input v-model="saveConfigForm.name"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-form-item prop="methods" label="| 请求方式"></el-form-item>
+            <el-select v-model="saveConfigForm.methods" style="width:100%">
+              <el-option label="GET" value="GET"></el-option>
+              <el-option label="POST" value="POST"></el-option>
+              <el-option label="DELETE" value="DELETE"></el-option>
+              <el-option label="PUT" value="PUT"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item prop="url" label="| 请求地址">
+            <el-input v-model="saveConfigForm.url"></el-input>
+          </el-form-item>
+          <el-form-item prop="header" label="| 请求headers">
+            <el-input v-model="saveConfigForm.header"></el-input>
+          </el-form-item>
+          <el-form-item prop="param" label="| 请求参数">
+            <el-input
+              v-model="saveConfigForm.param"
+              type="textarea"
+              :rows="5"
+              placeholder="输入请求参数json格式"
+            ></el-input>
+          </el-form-item>
+          <el-form-item prop="description" label="| 描述">
+            <el-input v-model="saveConfigForm.description"></el-input>
+          </el-form-item>
+        </el-form>
+        <div class="drawer__footer">
+          <el-button @click="addDrawer=false">取消</el-button>
+          <el-button @click="addSaveConfig">保存</el-button>
+        </div>
+      </div>
+    </el-drawer>
   </div>
 </template>
 
 <script>
+import { Loading } from 'element-ui'
 export default {
   data() {
     return {
@@ -93,15 +141,27 @@ export default {
       configList: [],
       total: 0,
       editDrawer: false,
-      adddrawer: false,
+      addDrawer: false,
       saveConfigForm: {
+        name: '',
         methods: '',
         url: '',
         header: '',
-        param: ''
+        param: '',
+        description: ''
+      },
+      saveConfigFormRule: {
+        name: [{ required: true, message: '请输入名称', trigger: 'blur' }],
+        methods: [
+          { required: true, message: '请选择请求方式', trigger: 'blur' }
+        ],
+        url: [{ required: true, message: '请输入url', trigger: 'blur' }],
+        header: [{ message: '请输入header', trigger: 'blur' }],
+        param: [{ required: true, message: '请输入参数', trigger: 'blur' }],
+        description: [{ message: '请输入描述', trigger: 'blur' }]
       },
       editConfigForm: {},
-      configDetailTitle: ''
+      loading: true
     }
   },
   created() {
@@ -112,13 +172,49 @@ export default {
       const { data: res } = await this.$http.post('configs', this.queryInfo)
       this.configList = res.api_config
       this.total = res._meta.total
+      this.loading = true
     },
     openConfigDetail(row) {
       this.editConfigForm = row
     },
-    async addConfig(id, name) {
-      this.adddrawer = true
-      this.configDetailTitle = name
+    addSaveConfig() {
+      this.$refs.saveConfigFormRef.validate(async valid => {
+        if (!valid) return
+        await this.$http
+          .post('addConfig', this.saveConfigForm)
+          .then(res => {
+            this.$message.success('添加配置成功')
+            this.addDrawer = false
+            this.$refs.saveConfigFormRef.resetFields()
+            this.getConfigList()
+          })
+          .catch(error => {
+            this.$message.error('添加配置失败')
+            this.$refs.saveConfigFormRef.resetFields()
+            console.log(error)
+          })
+      })
+    },
+    async release() {
+      await this.$http
+        .post('config/run/' + this.editConfigForm.id, {
+          methods: this.editConfigForm.methods,
+          url: this.editConfigForm.url,
+          header: this.editConfigForm.header,
+          param: this.editConfigForm.param
+        })
+        .then(res => {
+          this.$message.success('发布成功')
+          this.getConfigList()
+        })
+        .catch(error => {
+          this.$message.error('发布失败')
+          console.log(error)
+        })
+      this.loading = true
+    },
+    edit() {
+      console.log(this.editConfigForm)
     }
   }
 }
